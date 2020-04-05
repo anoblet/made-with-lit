257c8b3550bd8c81fcd2b732ae72bd340975e371
Andrew Noblet <andrewbnoblet@gmail.com>
2020-04-05 11:04:41 -0400

    chore(app): structure and testing

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./components/shell-component/shell-component";
+import { registerServiceWorker } from "@victorycto/web-utilities";
+
+const el = document.createElement("shell-component");
+document.body.appendChild(el);
+
+// Uncomment this to register service-worker
+// registerServiceWorker({ source: "/service-worker.js" });
