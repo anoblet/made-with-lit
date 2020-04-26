e7b0153d8c9c0f6063316e03cc95deafe42903dd
Andrew Noblet <andrewbnoblet@gmail.com>
2020-04-25 20:53:35 -0400

    chore(app): save

--- src/components/form-component/component.ts
+++ src/components/form-component/component.ts
@@ -2,37 +2,16 @@ import { addDocument } from "@anoblet/firebase";
 import "@material/mwc-list/mwc-list-item";
 import "@material/mwc-select";
 import "@material/mwc-textfield";
-import { customElement, LitElement, property, query } from "lit-element";
+import { customElement, LitElement, property } from "lit-element";
 import sharedStyles from "../../shared-styles";
 import style from "./style.css";
 import template from "./template.html";
 
-const categories = [
-    { label: "Component", value: "component" },
-    { label: "Component Suite", value: "component_suite" },
-];
-
 @customElement("form-component")
 export class FormComponent extends LitElement {
-    @property({ type: String }) author: string = "";
-    @property({ type: String }) category: string = "";
-    @property({ type: String }) github: string = "";
     @property({ type: Object }) model: any = {};
-    @property({ type: String }) organization: string = "";
-    @property({ type: String }) title: string = "";
-    @property({ type: String }) url: string = "";
 
     data: any = {};
-
-    addItem() {
-        addDocument("items", {
-            author: this.author,
-            category: this.category,
-            created: Date.now(),
-            title: this.title,
-            url: this.url,
-        });
-    }
 
     save() {
         addDocument("items", {
